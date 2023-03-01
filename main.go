a209a5fdf9e8ea0013a3f044248e6863ff132bdd
Emin Laletovic <laletovic@gmail.com>
2023-03-01 10:18:23 +0100

    added formatted Slack message

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 
-	"github.com/elaletovic/slacksnitch/queries"
+	"github.com/elaletovic/slacksnitch/models"
 	"github.com/sethvargo/go-githubactions"
 	"github.com/shurcooL/githubv4"
 	"github.com/slack-go/slack"
@@ -55,24 +54,25 @@ func main() {
 		"count": githubv4.Int(30),
 	}
 
-	err = githubClient.Query(context.Background(), &queries.VulnerabilityQuery, values)
+	var query models.VulnerabilityQuery
+
+	err = githubClient.Query(context.Background(), &query, values)
 	if err != nil {
 		githubactions.Fatalf("failed to call a vulnerability query, error :%v", err)
 	}
 
-	githubactions.Infof("vulnerability query: %v", queries.VulnerabilityQuery)
+	githubactions.Infof("vulnerability query: %v", query)
 
-	if queries.VulnerabilityQuery.Repository.VulnerabilityAlerts != nil {
-		for _, edge := range queries.VulnerabilityQuery.Repository.VulnerabilityAlerts.Edges {
+	if query.Repository.VulnerabilityAlerts != nil {
+		for _, edge := range query.Repository.VulnerabilityAlerts.Edges {
 			githubactions.Infof("node ID %v, severity %v", edge.Node.ID, edge.Node.SecurityAdvisory.Severity)
 		}
 	}
 
-	payload, err := json.Marshal(&queries.VulnerabilityQuery)
-	if err != nil {
-		githubactions.Fatalf("failed to marshal the vulnerability query, error :%v", err)
-	} else {
-		a, b, c, err := slackClient.SendMessage("security-alerts", slack.MsgOptionText(string(payload), true))
+	if query.Repository.VulnerabilityAlerts != nil && len(query.Repository.VulnerabilityAlerts.Edges) > 0 {
+
+		blocks := query.GetMessage()
+		a, b, c, err := slackClient.SendMessage("security-alerts", slack.MsgOptionBlocks(blocks.BlockSet...))
 		githubactions.Infof("a: %s, b: %s, c: %s, err: %v", a, b, c, err)
 	}
 }
